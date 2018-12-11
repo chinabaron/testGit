53a8f6cb00dfeec04d79960487b0e797e9bc7e03
ccitlw <839542864@qq.com>
2018-12-11 15:34:13 +0800

    111111111111111

--- src/gittest/hellogit.java
+++ src/gittest/hellogit.java
@@ -17,5 +17,6 @@ public class hellogit {
     public static void main(String[] args){
         System.out.println("hello github");
         System.out.println("hello github");
+        System.out.println("hello github");
     }
 }
