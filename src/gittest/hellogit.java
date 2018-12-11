86269c828024075866f32fbf82b5cd0eee144e5f
ccitlw <839542864@qq.com>
2018-12-11 10:20:58 +0800

    first commit

---
+++ src/gittest/hellogit.java
@@ -0,0 +1,20 @@
+/**
+ *Copyrigth (C),2015-2018 创原天地 
+ *FileName:hellogit
+ *Author:ccit
+ *Version V1.0
+ *Date:2018/12/10 18:41
+ *Desciption:11111111111111
+ *History:
+ *<Author>           <Time>           <Version>      <Desciption>
+ *
+ *
+ */
+
+package gittest;
+
+public class hellogit {
+    public static void main(String[] args){
+        System.out.println("hello github");
+    }
+}
